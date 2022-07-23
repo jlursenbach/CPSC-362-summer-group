316d1a204f7f5f6b8d9f7cbfc97962a9441a209d
goodpointy <91806141+goodpointy@users.noreply.github.com>
2022-07-22 20:52:25 -0700

    Update usingYahooAPI.py

--- Ass_3/usingYahooAPI.py
+++ Ass_3/usingYahooAPI.py
@@ -1,10 +1,16 @@
 import requests
 import json
+import pandas as pd
+import pandas_datareader as pdr
+import datetime as dt
 
 class stockData:
     def __init__(self):
         self.stockData = {"Opening Price": None, "Closing Price": None, "PE_ratio": None,
-                          "PS_ratio": None, "bid": None, "volume": "", "ask": None}
+                          "PS_ratio": None, "bid": None, "volume": "", "ask": None, "High": None,
+                          "Low": None}
+
+        self.historicalData = pd.DataFrame()
 
     def __str__(self):
         output = ""
@@ -13,14 +19,29 @@ class stockData:
 
         return output
 
-    def retrieveData(self):
+    def retrieveHistoricalData(self):
+        """getting historical stock data
+        for the back testing module"""
+        start = dt.datetime(2019, 1, 1)
+        end = dt.datetime(2022, 7, 22)
+
+        self.historicalData = pdr.get_data_yahoo(self.stockSymbol, start, end)
+
+        #reformatting dataframe to fit the required format for backtesting
+        self.historicalData.drop('Adj Close', axis=1, inplace=True)
+        self.historicalData = self.historicalData.reindex(columns=
+                                                          ['Open', 'High', 'Low', 'Close', 'Volume'])
+
+
+    def retrieveBasicData(self):
+        """retrieving basic stock data using yahoo finance api"""
         url = "https://yfapi.net/v6/finance/quote"
         querystring = {"symbols":"AAPL,USD"}
         headers = {'x-api-key': "pCJyFfGicf5sG8cbj16XO95hHDCtbqFNaqNuaWvn"}
         response = requests.request("GET", url, headers=headers, params=querystring)
 
-        requestedData = json.loads(response.text)
-        requestedData = requestedData["quoteResponse"]["result"][0]
+        #converting response.txt into a dictionary
+        requestedData = json.loads(response.text)["quoteResponse"]["result"][0]
 
 
         self.stockData["Opening Price"] = requestedData["regularMarketOpen"]
@@ -30,6 +51,8 @@ class stockData:
         self.stockData["bid"] = requestedData["bid"]
         self.stockData["volume"] = requestedData["regularMarketVolume"]
         self.stockData["ask"] = requestedData["ask"]
+        self.stockData["High"] = requestedData["regularMarketDayHigh"]
+        self.stockData["Low"] = requestedData["regularMarketDayLow"]
 
     def set_symbol(self, symbol: str):
         """
@@ -51,7 +74,8 @@ class stockData:
         """
 
         self.set_symbol(symbol)
-        self.retrieveData()
+        self.retrieveBasicData()
+        self.retrieveHistoricalData()
         return self.return_data()
 
 
@@ -62,6 +86,13 @@ def main():
     obj1 = stockData()
     obj1.scrape_stock("AAPL")
     print(obj1)
+    print(obj1.historicalData.columns.tolist())
+    obj1.historicalData = obj1.historicalData.reindex(columns =
+                                                      ['Open', 'High', 'Low', 'Close', 'Volume', 'Adj Close'])
+    #test = pd.DataFrame(data=obj1.stockData, index=[0])
+    #print(test)
+    #print(obj1.historicalData)
+    #print(obj1.historicalData['Date'])
 
 if __name__ == "__main__":
     main()
@@ -86,3 +117,4 @@ url = "https://yfapi.net/v11/finance/quoteSummary"
 querystring = {"symbols":"AAPL, assetProfile"}
 response = requests.request("GET", url, headers=headers, params=querystring)
 print(response.text)"""
+
