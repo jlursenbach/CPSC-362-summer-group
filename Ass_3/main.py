cc12764aa0fc94be2c94326515bb8377fcaeaf30
goodpointy <91806141+goodpointy@users.noreply.github.com>
2022-07-24 18:23:53 -0700

    Update main.py
    updated ui
    
    Signed-off-by: goodpointy <91806141+goodpointy@users.noreply.github.com>

--- Ass_3/main.py
+++ Ass_3/main.py
@@ -6,61 +6,85 @@ from pythonProjects.stockProgram.tentativePortfolio import tentative_Portfolio
 
 
 def main():
-    end = 'y'
     scrapper = stockData()
     userPortfolio = tentative_Portfolio()
-    while end == 'y':
-        #getting basic stock data and historical data
-
-        scrapper.scrape_stock(input("stock symbol: "))
-        #print(scrapper)
-        #print(scrapper.historicalData)
-
-        #conduct back testing
-        obj2 = startBackTesting()
-        obj2.getBackTestingResults(scrapper.historicalData)
-        #print(obj2.bt.run())
-        #obj2.bt.plot()
+    obj2 = startBackTesting()
 
 
-        choice = input("Do you wish to create a portfolio? y\\n: ")
-        if choice == 'y':
-            userPortfolio.buildPortfolio(scrapper.stockSymbol, scrapper.stockData, obj2.bt.run())
-            filename = input("save portfolio as: ")
+    help = "lookup <stock symbol>           gets data for selected stock\n" \
+           "createPortfolio <file name>     creates a portfolio\n" \
+           "update <file name>              add stock data to a portfolio after lookup\n" \
+           "load <file name>                loads a portfolio and displays it's contents\n" \
+           "exit                            exits program\n" \
+           "clear                           clears screen\n"
+
+
+    while True:
+        userInput = ""
+        userInput = input("enter command> ")
+        cmd = userInput.split()
+
+        if len(cmd) > 2:
+            print(f"[!] {str(userInput)} is not a recognized command")
+            continue
+
+        if cmd[0] == "lookup":
+
+            #getting basic stock data and historical data
+
+            scrapper.scrape_stock(cmd[-1])
+            #print(scrapper)
+            #print(scrapper.historicalData)
+
+            #conduct back testing
+
+            obj2.getBackTestingResults(scrapper.historicalData)
+            #print(obj2.bt.run())
+            #obj2.bt.plot()
+
+        elif cmd[0] == "createPortfolio":
+            userPortfolio.setPortfolio()
+            filename = cmd[-1]
             userPortfolio.savePortfolio(filename)
 
-        elif choice == 'n':
-            filename = input("enter portfolio name: ")
-            userPortfolio.loadPortfolio(filename)
+        elif cmd[0] == "update":
+            if scrapper.stockSymbol == "":
+                print("[!] error: a stock has not been selected for analysis yet")
+                continue
+            userPortfolio.loadPortfolio(cmd[-1])
+            userPortfolio.buildPortfolio(scrapper.stockSymbol, scrapper.stockData, obj2.bt.run())
+            userPortfolio.savePortfolio(cmd[-1])
+            print(f"[+] {scrapper.stockSymbol} stock data has been added to {cmd[-1]}")
 
-            choice = input("add another entry?: ")
-            if choice == 'y':
-                userPortfolio.buildPortfolio(scrapper.stockSymbol, scrapper.stockData, obj2.bt.run())
-                userPortfolio.savePortfolio(filename)
+        elif cmd[0] == "load":
+            userPortfolio.loadPortfolio(cmd[-1])
+            for key, value in userPortfolio.portfolio.items():
+                print(f"stock: {key}\n")
+                for key2, value2 in value.items():
+                    if type(value2) is dict:
+                        print(f'{key2}: ')
+                        for key3, value3 in value2.items():
+                            print(f'{key3}: {value3}')
 
+                        print()
 
-        end = input('cont?: ')
+                    else:
+                        print(f'{key2}: ')
+                        print(value2)
+                        print()
+                print("---------------------------------------------------------")
 
-    print()
-    #userPortfolio.loadPortfolio(filename=input("enter portfolio name: "))
-    #print(userPortfolio.portfolio)
-    for key, value in userPortfolio.portfolio.items():
-        print(f"stock: {key}\n")
-        for key2, value2 in value.items():
-            if type(value2) is dict:
-                print(f'{key2}: ')
-                for key3, value3 in value2.items():
-                    print(f'{key3}: {value3}')
+        elif cmd[0] == "exit":
+            exit()
 
-                print()
+        elif cmd[0] == 'help':
+            print(help)
 
-            else:
-                print(f'{key2}: ')
-                print(value2)
-                print()
+        elif cmd[0] == 'clear':
+            print("\n"*1000)
 
-
-
+        else:
+            print(f"[!] {userInput} is not a recognized command")
 
     #print(userPortfolio.portfolio)
 
