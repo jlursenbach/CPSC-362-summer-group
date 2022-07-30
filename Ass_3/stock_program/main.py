5eb585f30707213fd78942f68583ced037f402f1
goodpointy <91806141+goodpointy@users.noreply.github.com>
2022-07-29 18:23:08 -0700

    Update main.py
    Signed-off-by: goodpointy <91806141+goodpointy@users.noreply.github.com>

--- Ass_3/stock_program/main.py
+++ Ass_3/stock_program/main.py
@@ -1,76 +1,122 @@
+from pythonProjects.stockProgramV3.pullBasicStockData import getBasicStockData
+from pythonProjects.stockProgramV3.back_testing import startBackTesting
+from pythonProjects.stockProgramV3.tentative_portfolio import tentative_Portfolio
+from pythonProjects.stockProgramV3.getHistoricalData import retrieveHistoricalData
+from pythonProjects.stockProgramV3.baseClass import stockProgram
 
-#please make sure to change the path of these if you are running it on your computer
-from usingYahooAPI import stockData
-from back_testing import startBackTesting
-from tentativePortfolio import tentative_Portfolio
 
+
+def interfaceGetData(obj, symbol):
+    obj.setSymbol(symbol)
+    obj.getData()
+
+
+def interfaceBackTest(obj, strat, historicalData):
+    obj.setStratName(strat)
+    obj.setHistoricalData(historicalData)
+    obj.getData()
+
+def interfaceCreatePortfolio(obj, fileName):
+    obj.setPortfolio()
+    obj.savePortfolio(fileName)
+
+def interfaceUpdatePortfolio(obj, fileName, symbol, data, strategy = ""):
+    obj.loadPortfolio(fileName)
+    obj.setSymbol(symbol)
+    if symbol not in obj.portfolio:
+        obj.insertNewEntry()
+
+    if type(data) is dict:
+        obj.setBasicData(data)
+
+    else:
+        obj.setBackTestResults(data, strategy)
+
+    obj.savePortfolio(fileName)
+
+def interfaceLoadPortfolio(obj, fileName):
+    obj.loadPortfolio(fileName)
+    obj.output()
 
 def main():
-    end = 'y'
-    scrapper = stockData()
+    #base = stockProgram()
+    scrapper = getBasicStockData()
+    backtesting = startBackTesting()
     userPortfolio = tentative_Portfolio()
-    while end == 'y':
-        #getting basic stock data and historical data
-
-        scrapper.scrape_stock(input("stock symbol: "))
-        #print(scrapper)
-        #print(scrapper.historicalData)
-
-        #conduct back testing
-        obj2 = startBackTesting()
-        obj2.getBackTestingResults(scrapper.historicalData)
-        #print(obj2.bt.run())
-        #obj2.bt.plot()
+    histData = retrieveHistoricalData()
 
 
-        choice = input("Do you wish to create a portfolio? y\\n: ")
-        if choice == 'y':
-            userPortfolio.buildPortfolio(scrapper.stockSymbol, scrapper.stockData, obj2.bt.run())
-            filename = input("save portfolio as: ")
-            userPortfolio.savePortfolio(filename)
 
-        elif choice == 'n':
-            filename = input("enter portfolio name: ")
-            userPortfolio.loadPortfolio(filename)
-
-            choice = input("add another entry?: ")
-            if choice == 'y':
-                userPortfolio.buildPortfolio(scrapper.stockSymbol, scrapper.stockData, obj2.bt.run())
-                userPortfolio.savePortfolio(filename)
+    help = "lookup <stock symbol>           gets data for selected stock\n" \
+           "createPortfolio <file name>     creates a portfolio\n" \
+           "update <file name>              add stock data to a portfolio after lookup\n" \
+           "load <file name>                loads a portfolio and displays it's contents\n" \
+           "exit                            exits program\n" \
+           "clear                           clears screen\n"
 
 
-        end = input('cont?: ')
+    while True:
+        userInput = ""
+        userInput = input("enter command> ")
+        cmd = userInput.split()
 
-    print()
-    #userPortfolio.loadPortfolio(filename=input("enter portfolio name: "))
+        if len(cmd) > 6:
+            print(f"[!] {str(userInput)} is not a recognized command")
+            continue
+
+
+        if cmd[0] == "lookup":
+
+            #getting basic stock data and historical data
+            print("basic data: ")
+            interfaceGetData(scrapper, cmd[1])
+            print(scrapper)
+            print()
+
+
+
+        elif cmd[0] == "backtest":
+            #conducting back testing
+            interfaceGetData(histData, cmd[1])
+            interfaceBackTest(backtesting, cmd[2], histData.historicalData)
+            print("back testing results: ")
+            print(backtesting.bt.run())
+            print()
+            # obj2.bt.plot()
+            # obj2.bt.plot()
+
+
+        elif cmd[0] == "createPortfolio":
+            interfaceCreatePortfolio(userPortfolio, cmd[-1])
+
+        elif cmd[0] == "update":
+
+            if "-bd" in cmd and len(scrapper.stockData) > 0:
+                interfaceUpdatePortfolio(userPortfolio, cmd[1], cmd[2], scrapper.stockData)
+
+            if "-bt" in cmd:
+                interfaceUpdatePortfolio(userPortfolio, cmd[1], cmd[2], backtesting.bt.run(),
+                                         backtesting.strategyName)
+
+        elif cmd[0] == "load":
+            interfaceLoadPortfolio(userPortfolio, cmd[-1])
+
+
+        elif cmd[0] == "exit":
+            exit()
+
+        elif cmd[0] == 'help':
+            print(help)
+
+        elif cmd[0] == 'clear':
+            print("\n"*1000)
+
+        else:
+            print(f"[!] {userInput} is not a recognized command")
+
     #print(userPortfolio.portfolio)
-    for key, value in userPortfolio.portfolio.items():
-        print(f"stock: {key}\n")
-        for key2, value2 in value.items():
-            if type(value2) is dict:
-                print(f'{key2}: ')
-                for key3, value3 in value2.items():
-                    print(f'{key3}: {value3}')
-
-                print()
-
-            else:
-                print(f'{key2}: ')
-                print(value2)
-                print()
-
-
-
-
-    #print(userPortfolio.portfolio)
-
-
-
 
 if __name__ == '__main__':
     main()
 
-# test = pd.DataFrame(data=obj1.stockData, index=[0])
-# print(test)
-# print(obj1.historicalData['Date'])
-#print(obj1.historicalData.columns.tolist())
+
