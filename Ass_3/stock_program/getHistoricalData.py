7bc2b63f820ade7bb65cb54d7defa65452014052
goodpointy <91806141+goodpointy@users.noreply.github.com>
2022-07-29 18:26:41 -0700

    Add files via upload

---
+++ Ass_3/stock_program/getHistoricalData.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import pandas_datareader as pdr
+import datetime as dt
+
+class retrieveHistoricalData:
+    def __init__(self):
+        self.historicalData = pd.DataFrame()
+        self.stockSymbol = ""
+
+    def getData(self):
+        """getting historical stock data
+                for the back testing module"""
+        start = dt.datetime(2022, 4, 19)
+        end = dt.datetime(2022, 7, 22)
+
+        self.historicalData = pdr.get_data_yahoo(self.stockSymbol, start, end)
+
+        # reformatting dataframe to fit the required format for backtesting
+        self.historicalData.drop('Adj Close', axis=1, inplace=True)
+        self.historicalData = self.historicalData.reindex(columns=
+                                                          ['Open', 'High', 'Low', 'Close', 'Volume'])
+
+    def setSymbol(self, symbol):
+        self.stockSymbol = symbol
