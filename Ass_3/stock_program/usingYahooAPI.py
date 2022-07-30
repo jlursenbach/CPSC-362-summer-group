de24672c738682cf8580f417d086f7c753bb0760
goodpointy <91806141+goodpointy@users.noreply.github.com>
2022-07-29 18:24:16 -0700

    Update usingYahooAPI.py
    Signed-off-by: goodpointy <91806141+goodpointy@users.noreply.github.com>

--- Ass_3/stock_program/usingYahooAPI.py
+++ Ass_3/stock_program/usingYahooAPI.py
@@ -1,16 +1,9 @@
-import requests
-import json
-import pandas as pd
-import pandas_datareader as pdr
-import datetime as dt
+import requests, json
 
-class stockData:
+class getBasicStockData():
     def __init__(self):
-        self.stockData = {"Opening Price": None, "Closing Price": None, "PE_ratio": None,
-                          "PS_ratio": None, "bid": None, "volume": "", "ask": None, "High": None,
-                          "Low": None}
-
-        self.historicalData = pd.DataFrame()
+        self.stockData = {}
+        self.stockSymbol = ""
 
     def __str__(self):
         output = ""
@@ -19,83 +12,43 @@ class stockData:
 
         return output
 
-    def retrieveHistoricalData(self):
-        """getting historical stock data
-        for the back testing module"""
-        start = dt.datetime(2019, 1, 1)
-        end = dt.datetime(2022, 7, 22)
-
-        self.historicalData = pdr.get_data_yahoo(self.stockSymbol, start, end)
-
-        #reformatting dataframe to fit the required format for backtesting
-        self.historicalData.drop('Adj Close', axis=1, inplace=True)
-        self.historicalData = self.historicalData.reindex(columns=
-                                                          ['Open', 'High', 'Low', 'Close', 'Volume'])
-
-
-    def retrieveBasicData(self):
-        """retrieving basic stock data using yahoo finance api"""
+    def getData(self):
         url = "https://yfapi.net/v6/finance/quote"
-        querystring = {"symbols":"AAPL,USD"}
+        querystring = {"symbols": f"{self.stockSymbol},USD"}
         headers = {'x-api-key': "pCJyFfGicf5sG8cbj16XO95hHDCtbqFNaqNuaWvn"}
-        response = requests.request("GET", url, headers=headers, params=querystring)
+        try:
+            response = requests.request("GET", url, headers=headers, params=querystring)
 
-        #converting response.txt into a dictionary
+        except Exception:
+            pass
+
+        # converting response.txt into a dictionary
         requestedData = json.loads(response.text)["quoteResponse"]["result"][0]
+        # print(requestedData)
 
+        try:
+            self.stockData["Opening Price"] = requestedData["regularMarketOpen"]
+            self.stockData["Closing Price"] = requestedData["regularMarketPreviousClose"]
+            self.stockData["PE_ratio"] = requestedData["trailingPE"]
+            self.stockData["PS_ratio"] = requestedData["epsForward"]
+            self.stockData["bid"] = requestedData["bid"]
+            self.stockData["volume"] = requestedData["regularMarketVolume"]
+            self.stockData["ask"] = requestedData["ask"]
+            self.stockData["High"] = requestedData["regularMarketDayHigh"]
+            self.stockData["Low"] = requestedData["regularMarketDayLow"]
 
-        self.stockData["Opening Price"] = requestedData["regularMarketOpen"]
-        self.stockData["Closing Price"] = requestedData["regularMarketPreviousClose"]
-        self.stockData["PE_ratio"] = requestedData["trailingPE"]
-        self.stockData["PS_ratio"] = requestedData["epsForward"]
-        self.stockData["bid"] = requestedData["bid"]
-        self.stockData["volume"] = requestedData["regularMarketVolume"]
-        self.stockData["ask"] = requestedData["ask"]
-        self.stockData["High"] = requestedData["regularMarketDayHigh"]
-        self.stockData["Low"] = requestedData["regularMarketDayLow"]
+        except KeyError as missingKey:
+            print(f'[!] error: unable to obtain {missingKey}')
 
-    def set_symbol(self, symbol: str):
-        """
-        :param symbol:
-        :return:
-        """
+    def setSymbol(self, symbol):
         self.stockSymbol = symbol
 
-    def return_data(self) -> dict:
-        """
-        :return:
-        """
-        return {self.stockSymbol: self.stockData}
-
-    def scrape_stock(self, symbol: str) -> dict:
-        """
-        :param symbol:
-        :return:
-        """
-
-        self.set_symbol(symbol)
-        self.retrieveBasicData()
-        self.retrieveHistoricalData()
-        return self.return_data()
 
 
 
 
-def main():
 
-    obj1 = stockData()
-    obj1.scrape_stock("AAPL")
-    print(obj1)
-    print(obj1.historicalData.columns.tolist())
-    obj1.historicalData = obj1.historicalData.reindex(columns =
-                                                      ['Open', 'High', 'Low', 'Close', 'Volume', 'Adj Close'])
-    #test = pd.DataFrame(data=obj1.stockData, index=[0])
-    #print(test)
-    #print(obj1.historicalData)
-    #print(obj1.historicalData['Date'])
 
-if __name__ == "__main__":
-    main()
 
 
 
