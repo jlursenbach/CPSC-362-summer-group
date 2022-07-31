999700fd4e7fa2e88ac1774e00b92d00978ffaf9
goodpointy <91806141+goodpointy@users.noreply.github.com>
2022-07-30 20:41:34 -0700

    Update back_testing.py
    Signed-off-by: goodpointy <91806141+goodpointy@users.noreply.github.com>

--- Ass_3/stock_program/back_testing.py
+++ Ass_3/stock_program/back_testing.py
@@ -3,18 +3,32 @@ from backtesting.lib import crossover
 import pandas as pd
 import pandas_datareader as pdr
 from backtesting.test import SMA, GOOG
+import talib
+
+
+def decoratorGetData(method):
+    def wrapper(self):
+        if self.strategyName == "SmaCross":
+            self.strategy = SmaCross
+
+        elif self.strategyName == "Rsi":
+            self.strategy = rsiOscillator
+
+        method(self)
+
+    return wrapper
+
 
 
 class startBackTesting:
     def __init__(self):
         self.strategyName = ""
         self.historicalData = pd.DataFrame()
+        self.strategy = None
 
+    @decoratorGetData
     def getData(self):
-        strategy = ""
-        if self.strategyName == "SmaCross":
-            strategy = SmaCross
-        self.bt = Backtest(self.historicalData, strategy,
+        self.bt = Backtest(self.historicalData, self.strategy,
                            cash=10000, commission=.002,
                            exclusive_orders=True)
 
@@ -23,8 +37,6 @@ class startBackTesting:
 
     def setHistoricalData(self, data):
         self.historicalData = data
-
-
 
 
 class SmaCross(Strategy):
@@ -42,6 +54,22 @@ class SmaCross(Strategy):
             self.buy()
         elif crossover(self.sma2, self.sma1):
             self.sell()
+
+class rsiOscillator(Strategy):
+
+    upper = range(50, 85, 5)
+    lower = range(10, 45, 5)
+    rsiWindow = 14
+
+    def init(self):
+        self.rsi = self.I(talib.RSI, self.data.Close, self.rsiWindow)
+
+    def next(self):
+        if crossover(self.rsi, self.upper):
+            self.position.close()
+
+        elif crossover(self.lower, self.rsi):
+            self.buy()
 
 
 
