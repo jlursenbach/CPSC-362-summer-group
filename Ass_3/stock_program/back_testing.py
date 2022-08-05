8e81a50d64e21cc331a8def4765c992b5a379493
goodpointy <91806141+goodpointy@users.noreply.github.com>
2022-08-04 19:58:05 -0700

    Update back_testing.py
    Signed-off-by: goodpointy <91806141+goodpointy@users.noreply.github.com>

--- Ass_3/stock_program/back_testing.py
+++ Ass_3/stock_program/back_testing.py
@@ -4,8 +4,9 @@ import pandas as pd
 import pandas_datareader as pdr
 from backtesting.test import SMA, GOOG
 import talib
+import math
 
-
+#this was confirmed by the professor that it was implemented correctly
 def decoratorGetData(method):
     def wrapper(self):
         if self.strategyName == "SmaCross":
@@ -14,22 +15,26 @@ def decoratorGetData(method):
         elif self.strategyName == "Rsi":
             self.strategy = rsiOscillator
 
+        elif self.strategyName == "DCA":
+            self.strategy = DCA
+            self.historicalData = self.historicalData * (10 ** -6)
+
         method(self)
 
     return wrapper
 
 
 
-class startBackTesting:
+class StartBackTesting:
     def __init__(self):
         self.strategyName = ""
         self.historicalData = pd.DataFrame()
         self.strategy = None
+        self.cashAmount = 10000
 
     @decoratorGetData
     def getData(self):
-        self.bt = Backtest(self.historicalData, self.strategy,
-                           cash=10000, commission=.002,
+        self.bt = Backtest(self.historicalData, self.strategy, cash = self.cashAmount, commission=0.002,
                            exclusive_orders=True)
 
     def setStratName(self, name):
@@ -38,11 +43,14 @@ class startBackTesting:
     def setHistoricalData(self, data):
         self.historicalData = data
 
+    def setCashAmount(self, cash):
+        self.cashAmount = int(cash)
+
 
 class SmaCross(Strategy):
     """simple moving average crossover strat"""
-    n1 = 10
-    n2 = 20
+    n1 = 50
+    n2 = 100
 
     def init(self):
         close = self.data.Close
@@ -70,6 +78,28 @@ class rsiOscillator(Strategy):
 
         elif crossover(self.lower, self.rsi):
             self.buy()
+
+def decoratorInputAmountToInvest(method):
+    def wrapper(self):
+        self.amountToInvest = int(input("input amount to invest: "))
+
+        method(self)
+
+    return wrapper
+
+class DCA(Strategy):
+
+    amountToInvest = 10
+    @decoratorInputAmountToInvest
+    def init(self):
+
+        self.dayOfTheWeek = self.I(lambda x: x, self.data.Close.s.index.dayofweek)
+
+    def next(self):
+
+        if self.dayOfTheWeek[-1] == 1:
+            self.buy(size = math.floor(self.amountToInvest / self.data.Close[-1]))
+
 
 
 
