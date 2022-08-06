3839b4469db279d4c3ccc35e2f81cb044776eacc
goodpointy <91806141+goodpointy@users.noreply.github.com>
2022-08-05 20:08:49 -0700

    Update getHistoricalData.py
    Signed-off-by: goodpointy <91806141+goodpointy@users.noreply.github.com>

--- Ass_3/stock_program/getHistoricalData.py
+++ Ass_3/stock_program/getHistoricalData.py
@@ -3,11 +3,13 @@ import pandas_datareader as pdr
 import datetime as dt
 
 class RetrieveHistoricalData:
+    #this class is responsible for getting the historical data
     def __init__(self):
         self.historicalData = pd.DataFrame()
         self.stockSymbol = ""
 
     def getData(self):
+        #getting historical data
         """getting historical stock data
                 for the back testing module"""
         start = dt.datetime(2019, 4, 19)
